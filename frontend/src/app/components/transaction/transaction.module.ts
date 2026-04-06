3bae0a7a65e3e4c2165a8e09f6382a1f46aa8659
PHUM HOOMMAVONG <184559043+Phum7895@users.noreply.github.com>
2026-04-07 03:59:12 +0700

    Initialize knorrium/test_networks_config

---
+++ frontend/src/app/components/transaction/transaction.module.ts
@@ -0,0 +1,56 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { TransactionComponent } from './transaction.component';
+import { SharedModule } from '../../shared/shared.module';
+import { TxBowtieModule } from '../tx-bowtie-graph/tx-bowtie.module';
+import { GraphsModule } from '../../graphs/graphs.module';
+import { AcceleratePreviewComponent } from '../accelerate-preview/accelerate-preview.component';
+import { AccelerateFeeGraphComponent } from '../accelerate-preview/accelerate-fee-graph.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/',
+    pathMatch: 'full',
+  },
+  {
+    path: ':id',
+    component: TransactionComponent,
+    data: {
+      ogImage: true
+    }
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forChild(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class TransactionRoutingModule { }
+
+@NgModule({
+  imports: [
+    CommonModule,
+    TransactionRoutingModule,
+    SharedModule,
+    GraphsModule,
+    TxBowtieModule,
+  ],
+  declarations: [
+    TransactionComponent,
+    AcceleratePreviewComponent,
+    AccelerateFeeGraphComponent,
+  ]
+})
+export class TransactionModule { }
+
+
+
+
+
+
