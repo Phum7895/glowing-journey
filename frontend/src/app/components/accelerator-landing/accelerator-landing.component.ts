1887afeb4c28819962bc71fd4c39ddba26150d85
PHUM HOOMMAVONG <184559043+Phum7895@users.noreply.github.com>
2026-04-07 03:55:33 +0700

    Initialize hunicus/accelerator-mockup

---
+++ frontend/src/app/components/accelerator-landing/accelerator-landing.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { SeoService } from '../../services/seo.service';
+import { StateService } from '../../services/state.service';
+
+@Component({
+  selector: 'app-accelerator-landing',
+  templateUrl: './accelerator-landing.component.html',
+  styleUrls: ['./accelerator-landing.component.scss'],
+})
+export class AcceleratorLandingComponent implements OnInit {
+
+  constructor(
+  ) { }
+
+  ngOnInit() {
+
+  }
+
+}
