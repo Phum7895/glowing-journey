d69043843f62a6f669677b840a466fee2817ecef
PHUM HOOMMAVONG <184559043+Phum7895@users.noreply.github.com>
2026-04-07 04:01:11 +0700

    Initialize natsoni/fee-rounding

---
+++ frontend/src/app/components/fees-box/fees-box.component.ts
@@ -0,0 +1,80 @@
+import { Component, OnInit, ChangeDetectionStrategy, OnDestroy, ChangeDetectorRef } from '@angular/core';
+import { StateService } from '../../services/state.service';
+import { Observable, combineLatest, Subscription } from 'rxjs';
+import { Recommendedfees } from '../../interfaces/websocket.interface';
+import { feeLevels } from '../../app.constants';
+import { map, startWith, tap } from 'rxjs/operators';
+import { ThemeService } from '../../services/theme.service';
+
+@Component({
+  selector: 'app-fees-box',
+  templateUrl: './fees-box.component.html',
+  styleUrls: ['./fees-box.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class FeesBoxComponent implements OnInit, OnDestroy {
+  isLoading$: Observable<boolean>;
+  recommendedFees$: Observable<Recommendedfees>;
+  themeSubscription: Subscription;
+  gradient = 'linear-gradient(to right, var(--skeleton-bg), var(--skeleton-bg))';
+  noPriority = 'var(--skeleton-bg)';
+  fees: Recommendedfees;
+
+  constructor(
+    private stateService: StateService,
+    private themeService: ThemeService,
+    private cd: ChangeDetectorRef,
+  ) { }
+
+  ngOnInit(): void {
+    this.isLoading$ = combineLatest(
+      this.stateService.isLoadingWebSocket$.pipe(startWith(false)),
+      this.stateService.loadingIndicators$.pipe(startWith({ mempool: 0 })),
+    ).pipe(map(([socket, indicators]) => {
+      return socket || (indicators.mempool != null && indicators.mempool !== 100);
+    }));
+    this.recommendedFees$ = this.stateService.recommendedFees$
+      .pipe(
+        tap((fees) => {
+          this.fees = this.roundFees(fees);
+          this.setFeeGradient();
+        }
+      )
+    );
+    this.themeSubscription = this.themeService.themeChanged$.subscribe(() => {
+      this.setFeeGradient();
+    })
+  }
+
+  setFeeGradient() {
+    let feeLevelIndex = feeLevels.slice().reverse().findIndex((feeLvl) => this.fees.minimumFee >= feeLvl);
+    feeLevelIndex = feeLevelIndex >= 0 ? feeLevels.length - feeLevelIndex : feeLevelIndex;
+    const startColor = '#' + (this.themeService.mempoolFeeColors[feeLevelIndex - 1] || this.themeService.mempoolFeeColors[this.themeService.mempoolFeeColors.length - 1]);
+
+    feeLevelIndex = feeLevels.slice().reverse().findIndex((feeLvl) => this.fees.fastestFee >= feeLvl);
+    feeLevelIndex = feeLevelIndex >= 0 ? feeLevels.length - feeLevelIndex : feeLevelIndex;
+    const endColor = '#' + (this.themeService.mempoolFeeColors[feeLevelIndex - 1] || this.themeService.mempoolFeeColors[this.themeService.mempoolFeeColors.length - 1]);
+
+    this.gradient = `linear-gradient(to right, ${startColor}, ${endColor})`;
+    this.noPriority = startColor;
+
+    this.cd.markForCheck();
+  }
+
+  roundFees(fees: Recommendedfees): Recommendedfees {
+    fees.fastestFee = this.roundFeeValue(fees.fastestFee);
+    fees.halfHourFee = this.roundFeeValue(fees.halfHourFee);
+    fees.hourFee = this.roundFeeValue(fees.hourFee);
+    fees.economyFee = this.roundFeeValue(fees.economyFee);
+    fees.minimumFee = this.roundFeeValue(fees.minimumFee);
+    return fees;
+  }
+
+  roundFeeValue(fee: number): number {
+    return fee >= 10.0 ? Math.ceil(fee) : fee;
+  }
+
+  ngOnDestroy(): void {
+    this.themeSubscription.unsubscribe();
+  }
+}
