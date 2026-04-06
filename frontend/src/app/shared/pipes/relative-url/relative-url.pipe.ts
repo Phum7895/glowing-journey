49faa3d9aeae88472051c09015b571008841f0d0
PHUM HOOMMAVONG <184559043+Phum7895@users.noreply.github.com>
2026-04-07 03:58:58 +0700

    Initialize natsoni/various-fixes

---
+++ frontend/src/app/shared/pipes/relative-url/relative-url.pipe.ts
@@ -0,0 +1,24 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { StateService } from '../../../services/state.service';
+
+@Pipe({
+  name: 'relativeUrl'
+})
+export class RelativeUrlPipe implements PipeTransform {
+
+  constructor(
+    private stateService: StateService,
+  ) { }
+
+  transform(value: string, swapNetwork?: string): string {
+    let network = swapNetwork || this.stateService.network;
+    if (network === 'mainnet') network = '';
+    if (this.stateService.env.BASE_MODULE === 'liquid' && network === 'liquidtestnet') {
+      network = 'testnet';
+    } else if (this.stateService.env.BASE_MODULE !== 'mempool') {
+      network = '';
+    }
+    return (network ? '/' + network : '') + value;
+  }
+
+}
