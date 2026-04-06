38e71a66cfe4a325929bf4d0f321952d19cd018d
PHUM HOOMMAVONG <184559043+Phum7895@users.noreply.github.com>
2026-04-07 03:59:05 +0700

    Initialize mononaut/fix-tx-accel-label

---
+++ frontend/src/app/components/graphs/graphs.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { StateService } from '../../services/state.service';
+import { WebsocketService } from '../../services/websocket.service';
+
+@Component({
+  selector: 'app-graphs',
+  templateUrl: './graphs.component.html',
+  styleUrls: ['./graphs.component.scss'],
+})
+export class GraphsComponent implements OnInit {
+  flexWrap = false;
+
+  constructor(
+    public stateService: StateService,
+    private websocketService: WebsocketService
+  ) { }
+
+  ngOnInit(): void {
+    this.websocketService.want(['blocks']);
+
+    if (this.stateService.env.ACCELERATOR === true && (this.stateService.env.MINING_DASHBOARD === true || this.stateService.env.LIGHTNING === true)) {
+      this.flexWrap = true;
+    }
+  }
+}
---
+++ frontend/src/app/components/transaction/transaction.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { TransactionComponent } from './transaction.component';
+import { SharedModule } from '../../shared/shared.module';
+import { TxBowtieModule } from '../tx-bowtie-graph/tx-bowtie.module';
+import { GraphsModule } from '../../graphs/graphs.module';
+
+const routes: Routes = [
+  {
+    path: ':id',
+    component: TransactionComponent,
+    data: {
+      ogImage: true
+    }
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forChild(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class TransactionRoutingModule { }
+
+@NgModule({
+  imports: [
+    CommonModule,
+    TransactionRoutingModule,
+    SharedModule,
+    GraphsModule,
+    TxBowtieModule,
+  ],
+  declarations: [
+    TransactionComponent,
+  ]
+})
+export class TransactionModule { }
+
+
+
+
+
+
